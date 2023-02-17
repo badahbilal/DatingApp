08cafa847674bad379a113e3d342fe02e7874ca7
bbadah <bilal.badah@capgemini.com>
2023-02-17 15:21:13 +0100

    55. Persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,16 @@ export class AppComponent implements OnInit  {
   /**
    *
    */
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private accountService: AccountService){
     
   }
   
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers(){
     this.http.get("https://localhost:5001/api/users").subscribe({
       next: response => {
         this.users = response;
@@ -28,6 +35,13 @@ export class AppComponent implements OnInit  {
       error: error => console.log(error),
       complete: () => console.log("Request has completed")
     }) 
+  }
+  setCurrentUser(){
+    //const user: User = JSON.parse(localStorage.getItem('user')!)
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
   
 }
